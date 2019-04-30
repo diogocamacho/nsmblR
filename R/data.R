3857470296f70bff0320fbec62a30ef5eeac40d8
Diogo Camacho <diogocamacho@mordor.local>
2019-04-30 11:41:05 -0700

    added description of data sets. needs updating but works for now.

---
+++ R/data.R
@@ -0,0 +1,7 @@
+#' Regulon DB data
+#' Data on transcriptional regulators, small RNAs, regulatory interactions
+"regulondb"
+
+#' TTRUST data
+#' Data on TF -> Gene interactions in human annotated by TTRUST
+"ttrust"
