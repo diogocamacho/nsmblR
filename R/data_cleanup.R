732ec55bcba1413ac504c33568ce27dd5c1eb2a7
Diogo Camacho <diogocamacho@mordor.private.wireless.med.harvard.edu>
2019-05-07 13:36:20 -0400

    added data cleanup function

---
+++ R/data_cleanup.R
@@ -0,0 +1,32 @@
+#' Data cleanup
+#' 
+#' Function to clean up the data. The function will eliminate genes with no measurements, genes of low variance, and columns that have no measurement.
+#' 
+#' @param data Gene expression data matrix
+#' @param var_thr Percent variance of genes to be ignored. Eg, setting var_thr = 0.1 will remove the bottom 10% genes in terms of variance
+#' @return A list with ids of columns and rows to be removed before continuing analyses.
+data_cleanup <- function(data, var_thr) {
+  
+  if (missing(var_thr)) var_thr <- 0.1
+  
+  
+  # eliminate genes with no measurements
+  nix1 <- rowSums(data)
+  nix1 <- which(nix1 == 0)
+
+  # eliminate columns with no measurements
+  nix2 <- colSums(data)
+  nix2 <- which(nix2 == 0)
+
+  # keep only variant genes in the data
+  # to do that, eliminate the bottom 10% of genes in variance terms
+  nix3 <- sort(apply(data, 1, var), index.return = TRUE)
+  z <- ceiling(nrow(data) * var_thr)
+  nix3 <- nix3$ix[-seq(1, z)]
+
+  # list them
+  nix <- list(nix_cols = nix2,
+              nix_rows = union(nix1, nix3))
+  
+  return(nix)
+}
