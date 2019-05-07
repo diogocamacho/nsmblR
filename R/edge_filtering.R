aa96d13020a9a095eec80347a253499f300dca26
Diogo Camacho <diogocamacho@mordor.fios-router.home>
2019-05-07 18:06:15 -0400

    putting edge filtering into its own function

--- R/edge_filtering.R
+++ R/edge_filtering.R
@@ -2,20 +2,20 @@
 #' 
 #' This function performs a filtering of the inference results based on a quantile calculation of the results. This is currently hard-coded so that the function selects the top 10% edges in each of the inference algorithms.
 #' 
-#' @param ensemble_df An ensemble set of inference algorithms as defined in the \code{\link{merge_results}} function
+#' @param inferred_network An inferred networks as defined in the \code{\link{infer_network}} function
+#' @param quantile_thr Quantile based threshold to threshold inferred networks. Defaults to the top 10% set of edges.
 #' @return A filtered data frame with edges being quantified (1) or not (0) based on the quantiles for each of the methods.
-edge_filtering <- function(ensemble_df) {
+edge_filtering <- function(inferred_network, quantile_thr) {
   
-  fdf <- ensemble_df %>% 
-    dplyr::mutate(., cor = replace(cor, which(abs(cor) < quantile(cor, 0.9, na.rm = TRUE)), 0)) %>%
-    dplyr::mutate(., clr = replace(clr, which(clr < quantile(clr, 0.9, na.rm = TRUE)), 0)) %>%
-    dplyr::mutate(., aracne = replace(aracne, which(aracne < quantile(aracne, 0.9, na.rm = TRUE)), 0)) %>%
-    dplyr::mutate(., mrnet = replace(mrnet, which(mrnet < quantile(mrnet, 0.9, na.rm = TRUE)), 0)) %>%
-    dplyr::mutate(., mrnetb = replace(mrnetb, which(mrnetb < quantile(mrnetb, 0.9, na.rm = TRUE)), 0)) %>%
-    dplyr::mutate(., pcit = replace(pcit, which(pcit < quantile(pcit, 0.9, na.rm = TRUE)), 0)) %>%
-    dplyr::mutate(., mutrank = replace(mutrank, which(mutrank < quantile(mutrank, 0.9, na.rm = TRUE)), 0))
+  quants <- inferred_network %>%
+    dplyr::filter(., r != 0) %>% 
+    dplyr::select(., r) %>%
+    as.matrix %>%
+    as.vector %>%
+    quantile(probs = quantile_thr, na.rm = TRUE)
   
-  fdf[is.na(fdf)] <- 0
-  
-  return(fdf) # <-- filtered data frame given edge selection method
+  net <- inferred_network %>% 
+    tibble::add_column(., edge = 0) %>%
+    dplyr::mutate(., edge = replace(edge, which(r >= quants), 1))
+  return(net)
 }
