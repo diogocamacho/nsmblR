c5ec72464657e98f03aab37fdeb3889e3392fa76
Diogo Camacho <diogocamacho@mordor.fios-router.home>
2019-05-07 21:40:00 -0400

    editing nsmblr package description

--- R/nsmblR.R
+++ R/nsmblR.R
@@ -1,4 +1,7 @@
 #' nsmblR: Network inference ensembler
 #' 
+#' `nsmblR` is a package to perform inference of gene regulatory networks using an ensemble approach. `nsmblR` will take a gene expression compendium and infer the gene regulatory architecture using 7 popular network inference algorithms and then performing an edge count to find the consensus edges based on diverse voting methods.
 #' 
+#' @docType package
+#' @name nsmblR
 NULL
