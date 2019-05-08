a6dae619f5eb693565b02bf9e1f7970117cffaaf
Diogo Camacho <diogocamacho@mordor.private.wireless.med.harvard.edu>
2019-05-08 10:06:04 -0400

    cleaned up infer_networks function

--- R/infer_network.R
+++ R/infer_network.R
@@ -15,11 +15,13 @@
 #' 
 #' @param method Integer defining the method to be used. Defaults to using CLR (method 3).
 #' @param data Gene expression data frame or mutual information matrix
+#' @param quantile_thr Quantile threshold for edge assignment (defaults to 0.95)
 #' @return An inferred network matrix
-infer_network <- function(method = c(1, 2, 3, 4, 5, 6, 7), data) {
+infer_network <- function(method = c(1, 2, 3, 4, 5, 6, 7), data, quantile_thr) {
   
-  if(missing(method)) method <- 3
+  if (missing(method)) method <- 3
   if (missing(data)) stop("Need data matrix.")
+  if (missing(quantile_thr)) quantile_thr <- 0.95
   
   if (method == 1) {
     message("Method: Spearman correlations")
@@ -30,6 +32,19 @@ infer_network <- function(method = c(1, 2, 3, 4, 5, 6, 7), data) {
       dplyr::mutate(., y = replace(y, values = as.numeric(gsub("V", "", y))))  %>%
       dplyr::mutate(., r = replace(r, list = which(is.na(r)), values = 0)) %>% 
       dplyr::arrange(., x, y)
+    
+    quants <- net %>%
+      dplyr::filter(., r != 0) %>% 
+      dplyr::select(., abs(r)) %>%
+      as.matrix %>%
+      as.vector %>%
+      quantile(probs = quantile_thr, na.rm = TRUE)
+    
+    net <- net %>% 
+      tibble::add_column(., edge = 0) %>%
+      dplyr::mutate(., edge = replace(edge, which(abs(r) >= quants), 1)) %>% 
+      dplyr::select(., x, y, edge)
+    
   } else if (method == 2) {
     message("Method: PCIT")
     net <- netbenchmark::pcit.wrap(data = t(data)) %>%
@@ -40,6 +55,19 @@ infer_network <- function(method = c(1, 2, 3, 4, 5, 6, 7), data) {
       dplyr::mutate(., y = replace(y, values = as.numeric(gsub("V", "", y))))  %>%
       dplyr::mutate(., r = replace(r, list = which(is.na(r)), values = 0)) %>% 
       dplyr::arrange(., x, y)
+    
+    quants <- net %>%
+      dplyr::filter(., r != 0) %>% 
+      dplyr::select(., abs(r)) %>%
+      as.matrix %>%
+      as.vector %>%
+      quantile(probs = quantile_thr, na.rm = TRUE)
+    
+    net <- net %>% 
+      tibble::add_column(., edge = 0) %>%
+      dplyr::mutate(., edge = replace(edge, which(abs(r) >= quants), 1)) %>% 
+      dplyr::select(., x, y, edge)
+    
   } else if (method == 3) {
     message("Method: CLR")
     Z <- scale(data)
@@ -51,6 +79,19 @@ infer_network <- function(method = c(1, 2, 3, 4, 5, 6, 7), data) {
       dplyr::mutate(., y = replace(y, values = as.numeric(gsub("V", "", y))))  %>%
       dplyr::mutate(., r = replace(r, list = which(is.na(r)), values = 0)) %>% 
       dplyr::arrange(., x, y)
+    
+    quants <- net %>%
+      dplyr::filter(., r != 0) %>% 
+      dplyr::select(., r) %>%
+      as.matrix %>%
+      as.vector %>%
+      quantile(probs = quantile_thr, na.rm = TRUE)
+    
+    net <- net %>% 
+      tibble::add_column(., edge = 0) %>%
+      dplyr::mutate(., edge = replace(edge, which(r >= quants), 1)) %>% 
+      dplyr::select(., x, y, edge)
+    
   } else if (method == 4) {
     message("Method: ARACNe")
     net <- minet::aracne(mim = data, eps = 0.1) %>%
@@ -61,6 +102,19 @@ infer_network <- function(method = c(1, 2, 3, 4, 5, 6, 7), data) {
       dplyr::mutate(., y = replace(y, values = as.numeric(gsub("V", "", y))))  %>%
       dplyr::mutate(., r = replace(r, list = which(is.na(r)), values = 0)) %>% 
       dplyr::arrange(., x, y)
+    
+    quants <- net %>%
+      dplyr::filter(., r != 0) %>% 
+      dplyr::select(., r) %>%
+      as.matrix %>%
+      as.vector %>%
+      quantile(probs = quantile_thr, na.rm = TRUE)
+    
+    net <- net %>% 
+      tibble::add_column(., edge = 0) %>%
+      dplyr::mutate(., edge = replace(edge, which(r >= quants), 1)) %>% 
+      dplyr::select(., x, y, edge)
+    
   } else if (method == 5) {
     message("Method: MRNET")
     net <- minet::mrnet(data) %>%
@@ -71,6 +125,19 @@ infer_network <- function(method = c(1, 2, 3, 4, 5, 6, 7), data) {
       dplyr::mutate(., y = replace(y, values = as.numeric(gsub("V", "", y))))  %>%
       dplyr::mutate(., r = replace(r, list = which(is.na(r)), values = 0)) %>% 
       dplyr::arrange(., x, y)
+    
+    quants <- net %>%
+      dplyr::filter(., r != 0) %>% 
+      dplyr::select(., r) %>%
+      as.matrix %>%
+      as.vector %>%
+      quantile(probs = quantile_thr, na.rm = TRUE)
+    
+    net <- net %>% 
+      tibble::add_column(., edge = 0) %>%
+      dplyr::mutate(., edge = replace(edge, which(r >= quants), 1)) %>% 
+      dplyr::select(., x, y, edge)
+    
   } else if (method == 6) {
     message("Method: MRNETB")
     net <- minet::mrnetb(data) %>%
@@ -81,6 +148,19 @@ infer_network <- function(method = c(1, 2, 3, 4, 5, 6, 7), data) {
       dplyr::mutate(., y = replace(y, values = as.numeric(gsub("V", "", y)))) %>%
       dplyr::mutate(., r = replace(r, list = which(is.na(r)), values = 0)) %>% 
       dplyr::arrange(., x, y)
+    
+    quants <- net %>%
+      dplyr::filter(., r != 0) %>% 
+      dplyr::select(., r) %>%
+      as.matrix %>%
+      as.vector %>%
+      quantile(probs = quantile_thr, na.rm = TRUE)
+    
+    net <- net %>% 
+      tibble::add_column(., edge = 0) %>%
+      dplyr::mutate(., edge = replace(edge, which(r >= quants), 1)) %>% 
+      dplyr::select(., x, y, edge)
+    
   } else if (method == 7) {
     message("Method: MutRank")
     net <- netbenchmark::mutrank.wrap(data = t(data)) %>%
@@ -91,6 +171,19 @@ infer_network <- function(method = c(1, 2, 3, 4, 5, 6, 7), data) {
       dplyr::mutate(., y = replace(y, values = as.numeric(gsub("V", "", y))))  %>%
       dplyr::mutate(., r = replace(r, list = which(is.na(r)), values = 0)) %>% 
       dplyr::arrange(., x, y)
+    
+    quants <- net %>%
+      dplyr::filter(., r != 0) %>% 
+      dplyr::select(., r) %>%
+      as.matrix %>%
+      as.vector %>%
+      quantile(probs = quantile_thr, na.rm = TRUE)
+    
+    net <- net %>% 
+      tibble::add_column(., edge = 0) %>%
+      dplyr::mutate(., edge = replace(edge, which(r >= quants), 1)) %>% 
+      dplyr::select(., x, y, edge)
+    
   } else {
     stop("Unknown method.")
   }
