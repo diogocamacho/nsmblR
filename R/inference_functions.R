6823c9b8a9d9d6ec37d4c9eb629a1c531b3e0992
Diogo Camacho <dcamacho@valinor.cable.rcn.com>
2020-07-31 07:12:45 -0400

    -removed PCIT and MutRank methods

--- R/inference_functions.R
+++ R/inference_functions.R
@@ -51,32 +51,32 @@ spearman_wrapper <- function(data, quantile_thr) {
     dplyr::select(., x, y, edge)
 }
 
-pcit_wrapper <- function(data, quantile_thr) {
-  
-  if (missing(quantile_thr)) quantile_thr <- 0.95
-  
-  net <- netbenchmark::pcit.wrap(data = t(data)) %>%
-    corrr::as_cordf() %>%
-    corrr::shave(., upper = TRUE) %>%
-    corrr::stretch()  %>%
-    dplyr::mutate(., x = replace(x, values = as.numeric(gsub("V", "", x)))) %>% 
-    dplyr::mutate(., y = replace(y, values = as.numeric(gsub("V", "", y))))  %>%
-    dplyr::mutate(., r = replace(r, list = which(is.na(r)), values = 0)) %>% 
-    dplyr::arrange(., x, y)
-  
-  quants <- net %>%
-    dplyr::filter(., r != 0) %>% 
-    dplyr::select(., r) %>% 
-    abs %>%
-    as.matrix %>%
-    as.vector %>%
-    quantile(probs = quantile_thr, na.rm = TRUE)
-  
-  net <- net %>% 
-    tibble::add_column(., edge = 0) %>%
-    dplyr::mutate(., edge = replace(edge, which(abs(r) >= quants), 1)) %>% 
-    dplyr::select(., x, y, edge)
-}
+# pcit_wrapper <- function(data, quantile_thr) {
+#   
+#   if (missing(quantile_thr)) quantile_thr <- 0.95
+#   
+#   net <- netbenchmark::pcit.wrap(data = t(data)) %>%
+#     corrr::as_cordf() %>%
+#     corrr::shave(., upper = TRUE) %>%
+#     corrr::stretch()  %>%
+#     dplyr::mutate(., x = replace(x, values = as.numeric(gsub("V", "", x)))) %>% 
+#     dplyr::mutate(., y = replace(y, values = as.numeric(gsub("V", "", y))))  %>%
+#     dplyr::mutate(., r = replace(r, list = which(is.na(r)), values = 0)) %>% 
+#     dplyr::arrange(., x, y)
+#   
+#   quants <- net %>%
+#     dplyr::filter(., r != 0) %>% 
+#     dplyr::select(., r) %>% 
+#     abs %>%
+#     as.matrix %>%
+#     as.vector %>%
+#     quantile(probs = quantile_thr, na.rm = TRUE)
+#   
+#   net <- net %>% 
+#     tibble::add_column(., edge = 0) %>%
+#     dplyr::mutate(., edge = replace(edge, which(abs(r) >= quants), 1)) %>% 
+#     dplyr::select(., x, y, edge)
+# }
 
 aracne_wrapper <- function(data, quantile_thr) {
   
@@ -156,28 +156,28 @@ mrnetb_wrapper <- function(data, quantile_thr) {
     dplyr::select(., x, y, edge)
 }
 
-mutrank_wrapper <- function(data, quantile_thr) {
-  
-  if (missing(quantile_thr)) quantile_thr <- 0.95
-  
-  net <- netbenchmark::mutrank.wrap(data = t(data)) %>%
-    corrr::as_cordf() %>% 
-    corrr::shave(., upper = TRUE) %>%
-    corrr::stretch() %>%
-    dplyr::mutate(., x = replace(x, values = as.numeric(gsub("V", "", x)))) %>% 
-    dplyr::mutate(., y = replace(y, values = as.numeric(gsub("V", "", y))))  %>%
-    dplyr::mutate(., r = replace(r, list = which(is.na(r)), values = 0)) %>% 
-    dplyr::arrange(., x, y)
-  
-  quants <- net %>%
-    dplyr::filter(., r != 0) %>% 
-    dplyr::select(., r) %>%
-    as.matrix %>%
-    as.vector %>%
-    quantile(probs = quantile_thr, na.rm = TRUE)
-  
-  net <- net %>% 
-    tibble::add_column(., edge = 0) %>%
-    dplyr::mutate(., edge = replace(edge, which(r >= quants), 1)) %>% 
-    dplyr::select(., x, y, edge)
-}
+# mutrank_wrapper <- function(data, quantile_thr) {
+#   
+#   if (missing(quantile_thr)) quantile_thr <- 0.95
+#   
+#   net <- netbenchmark::mutrank.wrap(data = t(data)) %>%
+#     corrr::as_cordf() %>% 
+#     corrr::shave(., upper = TRUE) %>%
+#     corrr::stretch() %>%
+#     dplyr::mutate(., x = replace(x, values = as.numeric(gsub("V", "", x)))) %>% 
+#     dplyr::mutate(., y = replace(y, values = as.numeric(gsub("V", "", y))))  %>%
+#     dplyr::mutate(., r = replace(r, list = which(is.na(r)), values = 0)) %>% 
+#     dplyr::arrange(., x, y)
+#   
+#   quants <- net %>%
+#     dplyr::filter(., r != 0) %>% 
+#     dplyr::select(., r) %>%
+#     as.matrix %>%
+#     as.vector %>%
+#     quantile(probs = quantile_thr, na.rm = TRUE)
+#   
+#   net <- net %>% 
+#     tibble::add_column(., edge = 0) %>%
+#     dplyr::mutate(., edge = replace(edge, which(r >= quants), 1)) %>% 
+#     dplyr::select(., x, y, edge)
+# }
